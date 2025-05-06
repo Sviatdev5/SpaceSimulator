536c8bc15e766d1be04c04925d83438f4df3e72e
Sviat <romanivsvyatoslav2005@gmail.com>
2025-05-07 01:29:35 +0300

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { useContext } from "react"
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from "./components/Home"
 import ShipSection from "./components/ShipSection"
 import { useState, useEffect } from "react"
